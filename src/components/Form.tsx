7c880941f7c575b0ccb10f1364554601fd04b1e6
alfonsovillanueva <alfonso@villanuevagarcia.com>
2024-05-13 22:21:19 -0600

    ok

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,44 @@
-import { categories } from "../data/categories"
+import { ChangeEvent, useState } from "react";
+import { categories } from "../data/categories";
+import { Activity } from "../types";
 
 export default function Form() {
+
+    const [activity, setActivity] = useState<Activity>({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+
+        const isNumberFiel = ['category', 'calories'].includes(e.target.id);
+
+        console.log(isNumberFiel);
+        
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberFiel ? +e.target.value : e.target.value
+        })
+    }
+
+    const isValidActivity = ()=>{
+        
+    }
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category">Categoria:</label>
-            <select name="" id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white">
+            <select 
+                name="" 
+                id="category" 
+                className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                value={activity.category}
+                onChange={handleChange}
+            >
+                <option value="">-- Seleccione --</option>
                 {categories.map(category => (
                     <option
                         key={category.id}
@@ -17,12 +50,14 @@ export default function Form() {
             </select>
         </div>
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity">Actividad:</label>
+            <label htmlFor="name">Actividad:</label>
             <input 
                 type="text" 
-                id="activity" 
+                id="name" 
                 className="border border-slate-300 p-2 rounded-lg" 
                 placeholder="Ej. Comida, Jugo de Naranja, Ejercicio, Pesas, Etc"
+                value={activity.name}
+                onChange={handleChange}
             />
         </div>
         <div className="grid grid-cols-1 gap-3">
@@ -32,12 +67,14 @@ export default function Form() {
                 id="calories" 
                 className="border border-slate-300 p-2 rounded-lg" 
                 placeholder="Calorias, Ej. 300, 500"
+                value={activity.calories}
+                onChange={handleChange}
             />
         </div>
         <input 
             type="submit"
             className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-xl"
-            value="Guardar..."
+            value={"Guardar..."}
         />
     </form>
   )
